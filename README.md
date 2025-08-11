c8b358d028ad5e694f16946f541d0ae9836c8f24
Sergio Palacios <est.sergiod.palacios@unimilitar.edu.co>
2025-08-11 15:36:32 -0500

    Creación del primer notebook. EDO usando Euler

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ Repositorio de Aprendizaje de Simulación
 
 ---
 
+### Iniciar Entorno Virtual
+
+Inicializa el entorno virtual
+```bash
+python -m venv env
+```
+
+Instalar los modulos y librerias
+```bash
+pip install -r requirements.txt
+```
+
+
 ### Instrucciones para usar Jupyter Notebooks
 
 Ejecuta el siguiente comando en tu terminal para iniciar Jupyter Notebooks sin necesidad de un token de autenticación:
--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def function1(x,y):
-    return (x-np.exp(-x))*y
+def fwd_euler_while(f, a, b, y0, h):
+    t = a
+    y = y0
+    tvals = [t]
+    yvals = [y]
+    while t < b - 1e-12:
+        y += h*f(t, y)
+        t += h
+        tvals.append(t)
+        yvals.append(y)
+    return tvals, yvals
 
-def fwd_euler(f, a, b, y0, n):
-    h = (b-a)/n
-    print(h)
-    t = [a]
-    y = [y0]
-
-    # Usando For
+def fwd_for(f, a, b, y0, h):
+    n = round((b-a)/h)
+    h = (b-a)/n # fix if (b-a)/h
+    t = [0]*(n+1) # was not an int
+    y = [0]*(n+1)
+    t[0] = a
+    y[0] = y0
     for k in range(0, n):
-        y.append(y[k] + h*f(t[k],y[k]))
-        t.append(t[k] + h)
-    
-    """
-    # Usando While
-    yaux = y0
-    taux = a
-    while (t < b):
-        yaux += h*f(t, y)
-        taux += h
-        y.append(yaux)
-        t.append(taux)
-    """
-
+        y[k+1] = y[k] + h*f(t[k],y[k])
+        t[k+1] = t[k] + h
     return t, y
 
-X,Y = fwd_euler(function1, 0, 2, 2, 200)
+def edo1(t,y):
+    salida=-2*t*y
+    return salida
+
+def sol1(t):
+    y=np.exp(-t**2)
+    return y
+
+#t=np.arange(0,5,0.3)
+t2=np.linspace(0,4,101)
+yexacta= sol1(t2)
+
+taprox, yaprox=fwd_euler_while(edo1, 0, 4, 1, 0.3)
+
+plt.plot(t2,yexacta)
+plt.plot(taprox,yaprox,'*-')
+
+#plt.title("hola")
+plt.show()
