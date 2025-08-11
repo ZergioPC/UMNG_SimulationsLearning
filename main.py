3b2022861183e9562ea8f83080bf798e73ff8a9c
Sergio Palacios <est.sergiod.palacios@unimilitar.edu.co>
2025-08-11 16:47:50 -0500

    Inicia intento de creación de función Runge Kutta

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def fwd_euler_while(f, a, b, y0, h):
         yvals.append(y)
     return tvals, yvals
 
-def fwd_for(f, a, b, y0, h):
+def fwd_euler_for(f, a, b, y0, h):
     n = round((b-a)/h)
-    h = (b-a)/n # fix if (b-a)/h
-    t = [0]*(n+1) # was not an int
+    h = (b-a)/n         # fix if (b-a)/h was not an int
+    t = [0]*(n+1)           
     y = [0]*(n+1)
     t[0] = a
     y[0] = y0
@@ -24,6 +24,22 @@ def fwd_for(f, a, b, y0, h):
         y[k+1] = y[k] + h*f(t[k],y[k])
         t[k+1] = t[k] + h
     return t, y
+
+def fwd_runge_kutta_algoritm(f, a, b, x0, n):
+    h = round((b-a)/n)
+    t = np.linspace(a, b, np.floor((b-a)*h))
+    x = [x0]
+
+    for k in range(len(t-1)):
+        a = f(t[k],x[k])
+        b = f(t[k] + h/2, x[k] + a*h/2)
+        print(b)
+        c = f(t[k] + h/2, x[k] + b*h/2)
+        print(c)
+        d = f(t[k] + h, x[k] + h*c)
+        x.append(x[k] + h/6 * (a + 2*b + 2*c + d))
+
+    return t, x
 
 def edo1(t,y):
     salida=-2*t*y
@@ -37,10 +53,13 @@ def sol1(t):
 t2=np.linspace(0,4,101)
 yexacta= sol1(t2)
 
-taprox, yaprox=fwd_euler_while(edo1, 0, 4, 1, 0.3)
+#taprox, yaprox=fwd_euler_while(edo1, 0, 4, 1, 0.3)
+taprox, yaprox=fwd_runge_kutta_algoritm(edo1, 0, 4, 1, 0.3)
 
-plt.plot(t2,yexacta)
-plt.plot(taprox,yaprox,'*-')
+#plt.plot(t2,yexacta)
+#plt.plot(taprox,yaprox,'*-')
 
 #plt.title("hola")
-plt.show()
+#plt.show()
+
+print(len(taprox), len(yaprox))
