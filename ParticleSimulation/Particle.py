43559b369d57e6d069b33422bdc4ffd349f64752
Sergio Palacios <sergid26pal@gmail.com>
2025-09-25 19:02:14 -0500

    Simulación básica de partícula a partir de sumatoria de fuerzas

--- ParticleSimulation/Particle.py
+++ ParticleSimulation/Particle.py
@@ -1,10 +1,50 @@
 import numpy as np
 
 class Particula:
-    def __init__(self, x0:np.array, v0:np.array, masa:float):
-        y = np.concat((x0,v0))
-        m = masa
-        forces = []
+    """
+    Clase Partícula para simulación
+    """
+    def __init__(self, x0:np.array, v0:np.array, masa:float, h:float):
+        """        
+        Clase Partícula para simulación
 
-    def forceSolver(forces:np.array):
-        return np.sum(forces)
+        Args:
+           - x0 (np.Array)  : Posición inicial
+           - v0 (np.Array)  : Velocidad inicial
+           - masa (float)   : Masa del objeto
+           - h (float)      : Delta de tiempo en la operación
+        """
+        self.y = np.concat((x0,v0))     # [x, y, z, vx, vy, vz]
+        self.m = masa
+        self.F = np.array([0.0, 0.0, 0.0])
+        self.dt = h
+    
+    def __str__(self):
+        return f"Partícula: <{self.y[0]}, {self.y[1]}, {self.y[2]}>"
+    
+    def forcesSum(self, forces:np.array):
+        """
+        Suma todas las fuerzas que afectan sobre la partícula
+        
+        Args:
+           - forces (np.Array) Lista de fuerzas que afectan a la partícula
+        """
+        self.F = np.sum(forces, axis=0)
+    
+    def step(self):
+        """
+        Solucionador de la velocidad y la posición de la partícula según
+        la fuerza obtenida
+        """
+        x = self.y[:3]   # posición
+        v = self.y[3:]   # velocidad
+
+        a = self.F / self.m
+
+        # Update
+        v_new = v + a * self.dt
+        x_new = x + v_new * self.dt
+
+        # Save back
+        self.y[:3] = x_new
+        self.y[3:] = v_new
--- main.py
+++ main.py
@@ -4,13 +4,21 @@ import numpy as np
 from ParticleSimulation.Particle import Particula
 
 def simpleSimulation():
-    x0 = np.array([0.0, 1.0, 0.0])
-    v0 = np.array([0.0, 0.5, 0.0])
+    END = 10.0
+    time = 0.0
+    h = 0.1
 
-    test = Particula(x0,v0,1.0)
-    
-    while(True):
-        print("xd")
+    x0 = np.array([0.0, 100.0, 0.0])
+    v0 = np.array([0.0, 0.0, 0.0])
+
+    test = Particula(x0,v0,1.0,h)
+    gravedad = np.array([0.0, -9.8, 0.0])
+
+    while(time <= END):
+        test.forcesSum([gravedad])
+        test.step()
+        print(test)
+        time += h
 
 if __name__ == "__main__":
-    simpleSimulation() 
+    simpleSimulation()
